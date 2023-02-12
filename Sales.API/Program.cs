01fd77ee22551339e1c2ddb8a532c7822b7180e6
zuluaga94 <42913649+zuluaga94@users.noreply.github.com>
2023-02-12 18:37:19 -0500

    Add date Context

--- Sales.API/Program.cs
+++ Sales.API/Program.cs
@@ -1,15 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using Sales.API.Data;
+using Sales.Shared.Entities;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer("name=ConexionSales"));   
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
---
+++ Sales.Shared/Entities/Country.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sales.Shared.Entities
+{
+    public class Country
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Pais")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        [MaxLength(100, ErrorMessage = "El campo {0} no puede tener mas de {1} caracteres")]
+        public string Name { get; set; } = null!;
+
+
+       
+        
+    }
+}
