f5be7bc9bd67e4f73ea88ff2661a6dcf551ec30e
zuluaga94 <42913649+zuluaga94@users.noreply.github.com>
2023-02-18 15:34:35 -0500

    Begin Countries API controller

--- Sales.API/Program.cs
+++ Sales.API/Program.cs
@@ -25,4 +25,11 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+app.UseCors(x => x
+    .AllowAnyMethod()
+    .AllowAnyHeader()
+    .SetIsOriginAllowed(origin => true)
+    .AllowCredentials());
+
+
 app.Run();
