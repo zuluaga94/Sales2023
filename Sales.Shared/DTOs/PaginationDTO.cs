be90e4e38ca616f5437fb2b1ecb880b50a0699f8
zuluaga94 <42913649+zuluaga94@users.noreply.github.com>
2023-03-09 23:28:42 -0500

    All Complete pagination and filter

--- Sales.Shared/DTOs/PaginationDTO.cs
+++ Sales.Shared/DTOs/PaginationDTO.cs
@@ -6,8 +6,9 @@
 
         public int Page { get; set; } = 1;
 
-        public int RecordsNumber { get; set; } = 5;
+        public int RecordsNumber { get; set; } = 10;
 
         public string? Filter { get; set; }
+
     }
 }
